ff74ac0f521487b8e1c1f749324fc7e6e28ae0a8
Nitin Khare <kharenitin777@gmail.com>
2024-08-29 13:57:19 +0530

    added coin table ui with cash

--- src/Services/fetchCoinData.js
+++ src/Services/fetchCoinData.js
@@ -1,17 +1,14 @@
-import axiosInstance from "../Helpers/axiosInstance";
+import axiosInstance from "../helpers/axiosInstance";
 
-export async function fetchCoinData(page = 1, currency =  "usd") {
-    const perPage = 10;
-    try{
+export async function fetchCoinData(page = 1, currency) {
+    const perPage = 20;
+    try {
         const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`);
-        console.log(response);
-        return response;
-        
-    }
-    catch{
-        console.error("error");
+        // console.log(response.data);
+        return response.data;
+
+    } catch(error) {
+        console.error(error);
         return null;
-        
     }
 }
-
