7c65defca345442db5059d54df97d792c58fc222
Nitin Khare <kharenitin777@gmail.com>
2024-08-30 16:18:52 +0530

    started consuming coin details API

--- src/pages/CoinDetails.jsx
+++ src/pages/CoinDetails.jsx
@@ -1,12 +1,16 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { fetchCoinDetails } from "../Services/fetchCoinDetails";
+import parse from "html-react-parser";
+import currencyStore from "../ZustandStore/store";
 
 function CoinDetails(){
 
     const {coinId} = useParams();
 
-    const { data, isLoading, isError, error} = useQuery(['details', coinId], () => fetchCoinDetails(coinId), {
+    const {currency} = currencyStore();
+
+    const { data:coin, isLoading, isError, error} = useQuery(['coin', coinId], () => fetchCoinDetails(coinId), {
         // retry: 2,
         // retryDelay: 1000,
         cacheTime: 1000 * 60 * 2,
@@ -21,13 +25,53 @@ function CoinDetails(){
         return <div>Error: {error.message}</div>
     }
 
-    console.log(data.categories);
+    console.log(coin);
     
 
     return(
         <>
             <div className="mt-[64px]">
-                {coinId}
+                <div className="flex flex-col md:flex-row">
+
+                    <div className="md:w-1/3 wfull flex flex-col items-center mt-6 md:mt-0 border-r-2 border-gray-700">
+                        <img
+                            alt={coin?.name}
+                            src={coin?.image?.large }
+                            className="h-52 mb-5"
+                        />
+
+                        <h1 className="text-4xl font-bold mb-5">{coin?.name}</h1>
+
+                        <p className="w-full px-6 py-4 text-justify">
+                            {parse(coin?.description?.en)}
+                        </p>
+
+                        <div className="w-full flex flex-col md:flex-row md:justify-around">
+                            <div className="flex items-center mb-4 md:mb-0">
+                                <h2 className="text-xl font-bold">
+                                    Rank
+                                </h2>
+                                <span className="ml-3 text-xl">
+                                    {coin?.market_cap_rank}
+                                </span>
+                            </div>
+
+                            <div className="flex items-center mb-4 md:mb-0">
+                            <h2 className="text-xl text-yellow-400 font-bold">
+                                    Current Price
+                                </h2>
+                                <span className="ml-3 text-xl">
+                                    {coin?.market_data.current_price[currency]}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="md:w-2/3 w-full p-6">
+                        Coin Info
+                    </div>
+
+                </div>
             </div>
         </>
     )
